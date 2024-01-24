8685e1da3bd028215468545cd39e0415803b588c
Muratcan Agiç <muratcanagic@hotmail.com>
2024-01-24 13:11:59 +0300

    Stock report is done.

---
+++ FutureCafe.Business/Dtos/StockReportDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace FutureCafe.Business.Dtos
+{
+  public class StockReportDto
+  {
+    [DisplayName("Ürün Adı")]
+    public string ProductName { get; set; }
+
+    [DisplayName("Ürün Sayısı")]
+    public int ProductCount { get; set; }
+
+    [DisplayName("Değişim Tarihi")]
+    public DateTime CreatedDate { get; set; }
+  }
+}
